ed3c15fc9c13f5484a44e9f40400f4b616ac33a7
kkangda7 <hoon.kang@anex.co.jp>
2022-01-16 13:51:02 +0900

    todos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,143 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-do List</h2>
+    <input class="form-control" type="text" v-model="searchText" placeholder="Search" />
+    <hr />
+    <TodoSimpleForm @add-todo="addTodo" />
+    <div style="color : red">{{ error }}</div>
+    <div v-if="!filteredTodos.length">There's no todo added</div>
+    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" v-if="currentPage !==1">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage -1)">Previous</a>
+        </li>
+        <li
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''"
+          v-for="page in numberOfPages"
+          :key="page"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{ page }}</a>
+        </li>
+        <li class="page-item" v-if="numberOfPages !== currentPage">
+          <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from "vue";
+import TodoSimpleForm from "./components/TodoSimpleForm.vue";
+import TodoList from "./components/TodoList.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoSimpleForm,
+    TodoList,
+  },
+  setup() {
+    const todos = ref([]);
+    const error = ref("");
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+
+  
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit);
+    });
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
+        );
+        numberOfTodos.value = res.headers["x-total-count"];
+        todos.value = res.data;
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong.";
+      }
+    };
+
+    getTodos();
+
+    const addTodo = async (todo) => {
+      error.value = "";
+      try {
+        const res = await axios.post("http://localhost:3000/todos", {
+          subject: todo.subject,
+          completed: todo.completed,
+        });
+        todos.value.push(res.data);
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong.";
+      }
+    };
+
+    const toggleTodo = async (index) => {
+      error.value = "";
+      const id = todos.value[index].id;
+      try {
+        await axios.patch("http://localhost:3000/todos/" + id, {
+          completed: !todos.value[index].completed,
+        });
+        todos.value[index].completed = !todos.value[index].completed;
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong.";
+      }
+    };
+
+    const deleteTodo = async (index) => {
+      error.value = "";
+      const id = todos.value[index].id;
+      try {
+        await axios.delete("http://localhost:3000/todos/" + id);
+        todos.value.splice(index, 1);
+      } catch (err) {
+        console.log(err);
+        error.value = "Something went wrong.";
+      }
+    };
+
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    });
+
+
+    return {
+      todos,
+      addTodo,
+      deleteTodo,
+      toggleTodo,
+      searchText,
+      filteredTodos,
+      error,
+      numberOfPages,
+      currentPage,
+      getTodos,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.todo {
+  color: grey;
+  text-decoration: line-through;
 }
 </style>
